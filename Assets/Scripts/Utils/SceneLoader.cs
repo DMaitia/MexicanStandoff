6ea08b6188217a31c740ebff74e13ed481d46832
Darius Isamel Maitia Petros <dariusmaitia@gmail.com>
2019-11-04 19:35:54 -0300

    Settings UI

---
+++ Assets/Scripts/Utils/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Utils
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        public void Load(String sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
